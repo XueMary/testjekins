4d7720769e628aed90131e3ee8b4fa8a23cc92d1
jihong <632565970@qq.com>
2018-12-05 15:55:58 +0800

    test jenkins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    jenkins自动部署
   </div>
 </template>
 
